fbb79ce81107060bc95532d829de790ba566d992
Bleedingedge Studio <jazeb.jazi007@gmail.com>
2021-09-23 12:56:50 +0500

    redis configured for jwt tokens

--- apis/user.js
+++ apis/user.js
@@ -16,13 +16,23 @@ const User = require('../schema/user');
 const JWT_SECRET = process.env.JWT_SECRET;
 const salt = bcrypt.genSaltSync(10);
 
-const generateToken = user => jwt.sign(JSON.stringify(user), JWT_SECRET, { expiresIn: '1h' });
+const generateToken = user => jwt.sign(JSON.stringify(user), JWT_SECRET);
+const generateRefreshToken = user => jwt.sign(JSON.stringify(user), JWT_SECRET);
 
 const hashPwd = password => bcrypt.hashSync(password, salt);
 
+const verifyToken = token => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, JWT_SECRET, (err, result) => {
+            if (err) return (reject(err));
+            return resolve(result)
+        });
+    });
+}
+
 const saveInRedis = (key, token) => {
     client.set(key, token);
-    client.expire(key, 1 * 60 * 60);
+    client.expire(key, 1 * 60 * 60 * 60);
 }
 
 router.post('/signup', (req, res) => {
@@ -34,10 +44,10 @@ router.post('/signup', (req, res) => {
         if (err) return resp.success(res, null, err.message);
         delete user.password
         const token = generateToken(data);
-        saveInRedis(user.email, token);
-        
+        const refreshToken = generateRefreshToken({ jti:token });
+        saveInRedis(user.email, refreshToken);
 
-        return resp.success(res, { user, token });
+        return resp.success(res, { user, token, refreshToken });
     });
 });
 
@@ -50,8 +60,9 @@ router.post('/login', (req, res) => {
         if (!bcrypt.compareSync(password, user.password)) return resp.error(res, 'Invalid password');
 
         const token = generateToken(user);
-        saveInRedis(email, token);
-        return resp.success(res, { user, token });
+        const refreshToken = generateRefreshToken({ jti:token });
+        saveInRedis(email, refreshToken);
+        return resp.success(res, { user, token, refreshToken });
     }).catch(err => resp.error(res, err));
 });
 
@@ -74,7 +85,25 @@ router.post('/resetPassword', async (req, res) => {
     }
 });
 
-router.post('/refreshToken', (req, res) => {
+router.post('/refreshToken', async (req, res) => {
+    const { refreshToken } = req.body;
+    if(!refreshToken) return resp.error(res, 'Provide refresh token');
+    try {
+        const refToken = await verifyToken(refreshToken);
+        const user = await verifyToken(refToken.jti);
+        client.get(user.email, (err, data) => {
+            if(err) return console.error(err);
+            if(data){
+                const newToken = generateToken(data);
+                const newRefreshToken = generateRefreshToken({jti:newToken});
+                saveInRedis(user.email, newRefreshToken);
+                return resp.success(res, { user, token: newToken, refreshToken });
+            }
+    });
+    } catch (error) {
+        console.error(error);
+        return res.sendStatus(403);
+    }
     
 });
 
